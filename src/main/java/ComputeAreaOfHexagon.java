e2eae60c458de20ff1c3de473bf61ebea1ae4957
jzmnnava <146144314+jzmnnava@users.noreply.github.com>
2024-09-26 22:34:00 +0000

    Initial commit

---
+++ src/main/java/ComputeAreaOfHexagon.java
@@ -0,0 +1,19 @@
+//This program calculates the area of a hexagon
+
+import java.util.Scanner;
+
+public class ComputeAreaOfHexagon {
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+
+//Input number of sides
+    System.out.print("Enter the sides: ");
+    double s = input.nextDouble();
+
+//Calculate area
+    double area = (6*Math.pow(s,2))/(4*Math.tan(Math.PI/6));
+
+//Output
+    System.out.printf("The area of the hexagon is %4.2f\n", area);
+  }  
+}
